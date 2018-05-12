fbceee45d289dddffa51f3015260194bf666282c
linhaobin <linhaobin28@qq.com>
2018-05-12 19:05:13 +0800

    update  dependencies

--- listeners/on-logout.ts
+++ listeners/on-logout.ts
@@ -1,5 +1,5 @@
 import { log } from 'wechaty'
 
-export default async function onLogout(this, user) {
+export default async function onLogout(user: any) {
   log.info('Bot', `${user.name()} logouted`)
 }
